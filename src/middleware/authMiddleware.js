6454d637f983336a27af5a1497df0e51811f524f
Gaurav Singh Rawat <gr28437@gmail.com>
2026-09-01 15:23:32 +0530

    Fix production authentication cookie

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,6 +5,7 @@ const authMiddleware = async (req, res, next) => {
     try {
 
         const token = req.cookies.token
+        console.log(token)
 
         if (!token) {
             return res.status(401).json({
