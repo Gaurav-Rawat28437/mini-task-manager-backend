9b57665b00ded16bcfcfedf907934cc157388b0e
Gaurav Singh Rawat <gr28437@gmail.com>
2026-09-01 09:55:47 +0530

    Add logout API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -153,7 +153,28 @@ const login = async (req, res) => {
     }
 }
 
+
+const logout = async (req, res) => {
+    try {
+        res.clearCookie("token")
+
+        return res.status(200).json({
+            success: true,
+            message: "Logout successful"
+        })
+
+    } catch (error) {
+        console.error("Logout error:", error)
+
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
 module.exports = {
     register,
-    login
+    login,
+    logout
 }
